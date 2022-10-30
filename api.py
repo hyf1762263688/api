047f1f0487e5b5f625863bd293c369cb6224eee9
hyf1762263688 <116988276+hyf1762263688@users.noreply.github.com>
2022-10-30 20:31:20 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,10 @@
 import flask
 from flask import request, jsonify
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
+hf_model_path = 'IDEA-CCNL/Wenzhong-GPT2-110M'
+tokenizer = GPT2Tokenizer.from_pretrained(hf_model_path)
+model = GPT2LMHeadModel.from_pretrained(hf_model_path)
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -16,11 +20,29 @@ def home():
 def api_call():
     query_parameters = request.args
     input = query_parameters.get('input')
-    return jsonify(model(input))
+    return jsonify(call(input))
 
 
-def model(input):
-    return "out" + input
+def call(input):
+    question = input
+    inputs = tokenizer(question, return_tensors='pt')
+    generation_output = model.generate(**inputs,
+                                       return_dict_in_generate=True,
+                                       output_scores=True,
+                                       max_length=500,
+                                       # max_new_tokens=80,
+                                       do_sample=True,
+                                       top_p=0.8,
+                                       # num_beams=5,
+                                       eos_token_id=50256,
+                                       pad_token_id=0,
+                                       num_return_sequences=1)
+    output = ''
+    for idx, sentence in enumerate(generation_output.sequences):
+        output += 'next sentence %d:\n' % idx
+        output += tokenizer.decode(sentence).split('<|endoftext|>')[0]
+        output += '*' * 100
+    return output
 
 
 if __name__ == '__main__':
